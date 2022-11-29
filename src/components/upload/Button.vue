4817a2e63635a41a48afaa007f61d79f10e02ac6
sikeat <kobsikeat@outlook.com>
2022-11-29 12:50:09 +0700

    Initial Commit

---
+++ src/components/upload/Button.vue
@@ -0,0 +1,11 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  emits: ['click']
+})
+</script>
+
+<template>
+  <img src="./../../assets/upload.svg" alt="Gif button" width="10" height="10" class="ml-5 w-5 transition duration-200 filter hover:brightness-150 cursor-pointer transform hover:scale-110" @click="$emit('click')">
+</template>
